e4a5034c26068da2ded637e8ba7e343b510f99fc
Terry Murphy <tmurphyuw21@outlook.com>
2023-09-27 21:20:59 +0100

    FamilyTree challenge

--- CodingChallenge.FamilyTree/Program.cs
+++ CodingChallenge.FamilyTree/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CodingChallenge.FamilyTree;
+using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
@@ -6,7 +8,71 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var person = CreateFamilyTree();
+            var solution = new Solution();
+            var descendantNames = new List<string> { "Joe", "Jeep" };
+
+            foreach (var descendantName in descendantNames)
+            {
+                var birthMonth = solution.GetBirthMonth(person, descendantName);
+                if (string.IsNullOrEmpty(birthMonth))
+                {
+                    Console.WriteLine($"{descendantName} birth month is not found");
+                }
+                else
+                {
+                    Console.WriteLine($"{descendantName} birth month is on {birthMonth}");
+                }
+            }
+        }
+
+        private static Person CreateFamilyTree()
+        {
+            /*
+             * create a new tree that looks like this
+             *              Ted
+             *               /\
+             *              /  \
+             *           Jim  Sally
+             *           /      /\
+             *          /      /  \
+             *        Bob    Joe  George
+             */
+            return new Person
+            {
+                Name = "Ted",
+                Birthday = new DateTime(1950, 1, 1),
+                Descendants = new List<Person>
+                {
+                    new Person
+                    {
+                        Name = "Jim",
+                        Birthday = new DateTime(1970, 1, 1),
+                        Descendants = new List<Person>
+                        {
+                            new Person { Name = "Bob" }
+                        }
+                    },
+                    new Person
+                    {
+                        Name = "Sally",
+                        Birthday = new DateTime(1972, 1, 1),
+                        Descendants = new List<Person>
+                        {
+                            new Person
+                            {
+                                Name = "Joe",
+                                Birthday = new DateTime(1990, 1, 1)
+                            },
+                            new Person
+                            {
+                                Name = "George",
+                                Birthday = new DateTime(1992, 1, 1)
+                            }
+                        }
+                    }
+                }
+            };
         }
     }
 }
--- CodingChallenge.FamilyTree/Solution.cs
+++ CodingChallenge.FamilyTree/Solution.cs
@@ -6,7 +6,42 @@ namespace CodingChallenge.FamilyTree
     {
         public string GetBirthMonth(Person person, string descendantName)
         {
-            throw new NotImplementedException();
+            if (person == null)
+            {
+                throw new ArgumentNullException(nameof(person));
+            }
+
+            // compare current person's name with descendantName
+            if(string.Equals(person.Name, descendantName, StringComparison.OrdinalIgnoreCase))
+            {
+                // return the birth month
+                return person.Birthday.ToString("MMMM");
+            }
+
+            // the person has no descendants
+            // the end of the tree
+            if(person.Descendants.Count == 0)
+            {
+                return string.Empty;
+            }
+
+            // compare each descendant's name with descendantName
+            // if found, return the birth month
+            // otherwise, keep searching
+            // it will call itself recursively until it finds the descendantName
+            // or until it reaches the end of the tree
+            foreach (var child in person.Descendants)
+            {
+                var result = GetBirthMonth(child, descendantName);
+                if (!string.IsNullOrEmpty(result))
+                {
+                    // yeah, we found it
+                    return result;
+                }
+            }
+
+            // not found
+            return string.Empty;
         }
     }
 }
