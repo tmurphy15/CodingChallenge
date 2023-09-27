b27a1328eae36169c71145f8c703493346b143bf
Terry Murphy <tmurphyuw21@outlook.com>
2023-09-27 21:20:54 +0100

    PirateSpeak challenge

--- CodingChallenge.PirateSpeak/Solution.cs
+++ CodingChallenge.PirateSpeak/Solution.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Linq;
+using System.Collections.Generic;
 
 namespace CodingChallenge.PirateSpeak
 {
@@ -7,7 +7,39 @@ namespace CodingChallenge.PirateSpeak
     {
         public string[] GetPossibleWords(string jumble, string[] dictionary)
         {
-            throw new NotImplementedException();
+            var results = new List<string>();
+            // for each string in dictionary, check if it contains all the characters in jumble
+            // and if it does, add it to the results list, and assign that character in jumble to a space
+            // also check if the string in dictionary is the same length as jumble
+            foreach(var word in dictionary)
+            {
+                var jumbleArray = jumble.ToCharArray();
+
+                if (word.Length != jumble.Length)
+                {
+                    continue;
+                }
+                var isMatch = true;
+
+                foreach(var letter in word)
+                {
+                    var firstIndexInJumble = Array.IndexOf(jumbleArray, letter);
+                    if(firstIndexInJumble == -1)
+                    {
+                        isMatch = false;
+                        break;
+                    }
+
+                    jumbleArray[firstIndexInJumble] = ' ';
+                }
+
+                if(isMatch)
+                {
+                    results.Add(word);
+                }
+            }
+
+            return results.ToArray();
         }
     }
 }
